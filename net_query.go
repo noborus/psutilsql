2ad4a17d1ba5991f5e06b71e2671afc18c3d166b
Noboru Saito <noborusai@gmail.com>
2019-07-07 12:03:21 +0900

    net column split

--- net_query.go
+++ net_query.go
@@ -7,16 +7,41 @@ import (
 
 func NetQuery(query string, out trdsql.Format) error {
 	defaultQuery := "SELECT * FROM net"
-	var v interface{}
 
-	var err error
-	v, err = net.Connections("all")
+	conns, err := net.Connections("all")
 	if err != nil {
 		return err
+	}
+	type wrapConnection struct {
+		Fd        uint32
+		Family    uint32
+		Type      uint32
+		LaddrIP   string
+		LaddrPort uint32
+		RaddrIP   string
+		RaddrPort uint32
+		status    string
+		Uids      []int32
+		Pid       int32
+	}
+	data := make([]wrapConnection, len(conns))
+	for i, conn := range conns {
+		c := wrapConnection{}
+		c.Fd = conn.Fd
+		c.Family = conn.Family
+		c.Type = conn.Type
+		c.LaddrIP = conn.Laddr.IP
+		c.LaddrPort = conn.Laddr.Port
+		c.RaddrIP = conn.Raddr.IP
+		c.RaddrPort = conn.Raddr.Port
+		c.status = conn.Status
+		c.Uids = conn.Uids
+		c.Pid = conn.Pid
+		data[i] = c
 	}
 	if query == "" {
 		query = defaultQuery
 	}
-	return SliceQuery(v, "net", query, out)
+	return SliceQuery(data, "net", query, out)
 
 }
